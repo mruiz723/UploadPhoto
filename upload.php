e37ead16e0dec42d65fcd57580ac784a338ce805
mruiz723 <mruiz723@gmail.com>
2016-02-18 16:19:23 -0500

    Add hola.php and upload.php

---
+++ upload.php
@@ -0,0 +1,31 @@
+<?php
+$password = $_POST["password"];
+$email = $_POST["email"];
+$userId = $_POST["userId"];
+
+$target_dir = "wp-content/uploads/2016/02";if(!file_exists($target_dir))
+{
+mkdir($target_dir, 0777, true);
+}
+
+$target_dir = $target_dir . "/" . basename($_FILES["file"]["name"]);
+
+if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_dir)) 
+{
+echo json_encode([
+"Message" => "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.",
+"Status" => "OK",
+"userId" => $_REQUEST["userId"],
+"url" => "http://$_SERVER[HTTP_HOST]/$target_dir"
+]);
+
+} else {
+
+echo json_encode([
+"Message" => "Sorry, there was an error uploading your file.",
+"Status" => "Error",
+"userId" => $_REQUEST["userId"]
+]);
+
+}
+?>
